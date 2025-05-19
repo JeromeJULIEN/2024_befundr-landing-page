d24bb7003c09c8be30c58c0b299dd918649cb93f
JeromeJULIEN <102427619+JeromeJULIEN@users.noreply.github.com>
2025-05-19 14:02:28 +0200

    add text to partner section

--- src/components/_PartnerSection.tsx
+++ src/components/_PartnerSection.tsx
@@ -21,6 +21,16 @@ const PartnerSection = () => {
         </p>
 
         <CarouselPartner />
+        <h3 className="textStyle-headline w-full md:w-2/3 text-center mt-6">
+          Our partners play a fundamental role in{" "}
+          <strong className="text-accent">the selection and success</strong> of
+          projects{" "}
+        </h3>
+        <h3>
+          They provide continuous,{" "}
+          <strong className="!text-accent">strategic support</strong> throughout
+          the incubation
+        </h3>
       </div>
     </AnimatedBlock>
   );
