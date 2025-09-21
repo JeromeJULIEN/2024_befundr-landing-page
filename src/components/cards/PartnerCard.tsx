67b8e797dc253deb5c62663dc2426858f539cb6b
JeromeJULIEN <102427619+JeromeJULIEN@users.noreply.github.com>
2025-09-21 21:08:14 +0200

    css fix

--- src/components/cards/PartnerCard.tsx
+++ src/components/cards/PartnerCard.tsx
@@ -11,7 +11,7 @@ const PartnerCard = ({ partner }: { partner: Partner }) => {
         height={100}
         className="rounded-full mb-4 aspect-square"
       />
-      <h3 className="h3Style mb-2 text-center">{partner.name}</h3>
+      <h3 className="h4Style md:h3Style mb-2 text-center">{partner.name}</h3>
       <p className="hidden md:block bodyStyle text-center w-[190px] md:w-[290px] line-clamp-1 md:line-clamp-2">
         {partner.description}
       </p>
